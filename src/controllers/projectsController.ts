97a93e7eeaee440a86cfa19880c8742b271304cc
Sheldon Hart <sheldon.hart88@gmail.com>
2025-04-14 21:19:54 +0200

    Add CDN cache control header to GitHub projects response

--- src/controllers/projectsController.ts
+++ src/controllers/projectsController.ts
@@ -28,6 +28,7 @@ export const getGitHubProjectsByUsername = async (
   );
 
   reply.header("Cache-Control", "public, max-age=600, s-maxage=600");
+  reply.header("CDN-Cache-Control", "max-age=600");
 
   if (!gitHubProjects || gitHubProjects.length === 0) {
     return reply.code(404).send({ error: "GitHub Projects not found" });
