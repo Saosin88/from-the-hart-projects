6ebd9e31e8e83d2ca335c033a8c845ae0b2e810a
Sheldon Hart <sheldon.hart88@gmail.com>
2025-10-24 08:42:12 +0200

    chore: update type provider dependency from @fastify/type-provider-typebox to @sinclair/typebox and adjust imports

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import { registerSwagger } from "./config/swagger";
 
 import { fastifyLogger } from "./config/logger";
 import projectRoutes from "./routes/projects";
+import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 
 export function buildApp(): FastifyInstance {
   const app = fastify({
     logger: fastifyLogger,
   });
+
+  app.withTypeProvider<TypeBoxTypeProvider>();
 
   registerSwagger(app);
 
